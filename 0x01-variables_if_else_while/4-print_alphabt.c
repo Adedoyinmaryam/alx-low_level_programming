cb97607285a2fba34e61a47e094bb8bfd4df5761
Adedoyinmaryam <maryamakindele81@gmail.com>
2023-08-13 09:13:25 -0700

    Task 4

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: alway returns 0
+ *
+ */
+int main(void)
+{
+	char a;
+
+	a = 'a';
+	while (a = 'a'; a <= 'z')
+	{
+		if (a == 'q' || a == 'e')
+		{
+		continue;
+		}
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
+
