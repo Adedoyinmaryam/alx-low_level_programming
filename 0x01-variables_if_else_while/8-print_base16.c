5995f459e5dc83bea89a29b795d94d26308a6811
Adedoyinmaryam <maryamakindele81@gmail.com>
2023-08-14 07:31:20 -0700

    Task 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+int main(void)
+{
+	char letter;
+
+	for (a = '0'; a < 10; a++)
+	{
+		putchar(a);
+	}
+	for (a = 'a'; a <= 'f'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
+
+
