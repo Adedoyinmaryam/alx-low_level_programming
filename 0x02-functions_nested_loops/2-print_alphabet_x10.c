bf226a0677cbe0273f1f241a3155283d595cc3ed
Adedoyinmaryam <maryamakindele81@gmail.com>
2023-08-18 03:53:28 -0700

    Task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * print_alphabet - prints alphabet
+ * print_alphabet_x10 - prints alphabet 10 times
  *
  * Return: always 0;
  *
@@ -16,7 +16,6 @@ void print_alphabet_x10(void)
 		{
 			_putchar(a);
 		}
-	}
 	_putchar('\n');
-	
+	}
 }
