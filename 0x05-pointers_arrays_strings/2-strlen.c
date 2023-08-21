abc1482fd85c592b51da9585c6503cd25b86f63c
Adedoyinmaryam <maryamakindele81@gmail.com>
2023-08-21 08:28:38 -0700

    Task 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+ * _strlen - gets character string
+ *
+ * @s: character to getstring
+ * Return: returns length
+ */
+int _strlen(char *s)
+{
+	int len;
+
+	*s = {};
+	len = _strlen(*s);
+	printf("strlen(const char *__s) :  %d\n", len);
+	return (0);
+}
+
+
+
