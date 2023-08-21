df938d65d9815ab3d296b2468795a2e1ab5f211b
Adedoyinmaryam <maryamakindele81@gmail.com>
2023-08-21 10:02:20 -0700

    Task

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -2,16 +2,10 @@
 #include "2-strlen.c"
 void print_rev(char *s)
 {
-	int length = strlen(input_string);
-    for (int i = length - 1; i >= 0; i--) {
-        _putchar("%c", input_string[i]);
+	int i;
+
+    for (i = _strlen(s) - 1; i >= 0; i--)
+    {
+        _putchar(*(s + i));
     }
-    _putchar("\n");
-}
-
-int main() {
-    const char *input_string = "Hello, World!";
-    print_reverse_string(input_string);
-    return 0;
-}
-
+    _putchar('\n')
