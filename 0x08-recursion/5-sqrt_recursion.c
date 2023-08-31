0b62fa0077f3da9815489b7f2fb77efcdcc00d4e
Adedoyinmaryam <maryamakindele81@gmail.com>
2023-08-31 09:50:04 -0700

    Task

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,3 +1,4 @@
+#include "main.h"
 /**
  * _sqrt_helper - Helper function to calculate the square root recursively.
  * @n: The input number.
@@ -38,10 +39,10 @@ int _sqrt_helper(int n, int start, int end)
 int _sqrt_recursion(int n)
 {
 	if (n == 0 || n == 1)
-    {
-        return n;
-    }
+	{
+	return (n);
+	}
 
     /* Recursive case: Calculate the square root */
-    return _sqrt_helper(n, 1, n);
+	return (_sqrt_helper(n, 1, n));
 }
