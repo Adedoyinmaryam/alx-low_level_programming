c444d943fdff166d7f84915fbc38c4265c1691d8
Adedoyinmaryam <maryamakindele81@gmail.com>
2023-08-31 08:43:25 -0700

    Task 0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -13,17 +13,5 @@ void _puts_recursion(char *s)
 	_putchar(*s);
 	_puts_recursion(s + 1);
 }
-/**
- * main - Entry point of the program.
- * Return: Always 0.
- */
-int main(void)
-{
-	char str[] = "Hello Recursion";
-
-	_puts_recursion(str);
-	return (0);
-}
-
 
 
