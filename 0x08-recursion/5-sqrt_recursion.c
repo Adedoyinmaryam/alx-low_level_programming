a856519e9d8bda7e479a5c969b5b90bb2a1c980b
Adedoyinmaryam <maryamakindele81@gmail.com>
2023-08-31 09:59:18 -0700

    Task

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -38,9 +38,9 @@ int _sqrt_helper(int n, int start, int end)
  */
 int _sqrt_recursion(int n)
 {
-	if (n == 0 || n == 1)
+	if (n == 0)
 	{
-	return (n);
+		return (0);
 	}
 
     /* Recursive case: Calculate the square root */
