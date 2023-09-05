889c177327c04ca28a42266e8a18afeb0e1c0b27
Adedoyinmaryam <maryamakindele81@gmail.com>
2023-09-05 09:31:17 -0700

    Task

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,12 +10,16 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
-	if (grid != NULL)
+	if (height <= 0)
+		return;
+
+	if (grid == NULL)
+		return;
+
+	for (i = 0; i < height; i++)
 	{
-		for (i = 0; i < height; i++)
-		{
-			free(grid[i]);
-		}
-		free(grid);
+		free(grid[i]);
 	}
+
+	free(grid);
 }
