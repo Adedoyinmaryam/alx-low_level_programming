9c4b5f963fa4ba275fe5b08edee46ee0c64de62d
Adedoyinmaryam <maryamakindele81@gmail.com>
2023-09-07 11:48:36 -0700

    Task

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -25,7 +25,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 			return (NULL);
 		return (new_ptr);
 	}
-	if (new_size = 0 && ptr != NULL)
+	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
@@ -35,7 +35,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 		return (ptr);
 	}
 	new_ptr = malloc(new_size);
-	if (new_ptr = NULL)
+	if (new_ptr == NULL)
 	{
 		return (NULL);
 	}
