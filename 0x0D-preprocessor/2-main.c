00d4c23fc1142ca89e66abc191743aaf36b91fcb
Adedoyinmaryam <maryamakindele81@gmail.com>
2023-09-11 05:33:00 -0700

    Task

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,4 +1,11 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: Prints the name of the source file
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	printf("File Name: %s\n", __FILE__);
