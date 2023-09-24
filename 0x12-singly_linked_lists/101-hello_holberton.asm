7cf983627dbd8a66004444fcf3f085c46f0f7df7
Adedoyinmaryam <maryamakindele81@gmail.com>
2023-09-24 08:31:33 -0700

    Task

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,15 +1,9 @@
-section .data
-	hello db "Hello, Holberton",10,0 ; The string to print with a newline
-
-section .text
-	global main
-
+ global    main
+          extern    printf
 main:
-	; Call printf
-	mov rdi, hello ; Set the format string
-	call printf     ; Call the printf function;
-
-	; Exit the program
-	mov rax, 60    ; syscall number for exit
-	xor rdi, rdi   ; Exit status 0
-	syscall
+	  mov   edi, format
+	  xor   eax, eax
+	  call  printf
+	  mov 	eax, 0
+	  ret
+format: db 'Hello, Holberton\n',0
