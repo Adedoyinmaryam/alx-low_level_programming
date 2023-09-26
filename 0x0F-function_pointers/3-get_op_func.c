8baf4b17ae8f3832f0aaf5ac2506e1261f94c5ba
Adedoyinmaryam <maryamakindele81@gmail.com>
2023-09-26 14:14:14 -0700

    Task

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -17,14 +17,14 @@ int (*get_op_func(char *s))(int, int)
 		{"/", op_div},
 		{"%", op_mod},
 		{NULL, NULL}
-};
+	};
 	int i;
 
 	i = 0;
-	while (ops[i].f != NULL)
-	{
-		if (*s == *(ops[i].op) && s[1] == '\0')
-			return (ops[i].f);
+
+	while (ops[i].op != NULL && *(ops[i].op) != *s)
 		i++;
-	}
+
+	return (ops[i].f);
+}
 }
