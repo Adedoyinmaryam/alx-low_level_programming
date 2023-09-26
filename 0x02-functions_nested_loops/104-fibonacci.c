c741c22fc32b5b702c85db896e35c9bb42923caa
Adedoyinmaryam <maryamakindele81@gmail.com>
2023-09-26 14:48:02 -0700

    Task

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-
-	/**
-        * main - main block
-        * Description: Find and print the first 98 fib numbers starting with 1 and 2.
-        * Numbers should be coma and space separated.
-	* 
-	* return: 0
-	*/
+/**
+* main - main block
+* Description: Find and print the first 98 fib numbers starting with 1 and 2.
+* Numbers should be coma and space separated.
+*
+* Return: 0
+*/
 int main(void)
 {
 	unsigned long int i, j, k, j1, j2, k1, k2;
