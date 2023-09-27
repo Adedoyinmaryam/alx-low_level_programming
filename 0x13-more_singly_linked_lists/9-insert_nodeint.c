3ca33769d54ac5ca43d9117a749a43a18d7763a4
Adedoyinmaryam <maryamakindele81@gmail.com>
2023-09-27 05:25:34 -0700

    Task

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * insert_nodeint_at_index - Inserts a new node at a given position in a linked list
+ * insert_nodeint_at_index - Inserts a new node at a given position
  * @head: A pointer to a pointer to the head of the linked list
  * @idx: The index at which to insert the new node (starting at 0)
  * @n: The integer value to be stored in the new node
