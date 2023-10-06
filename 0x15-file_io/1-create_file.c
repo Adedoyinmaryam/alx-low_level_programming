c201a87a51cc4f0e546f97060afc491ff97cff21
Adedoyinmaryam <maryamakindele81@gmail.com>
2023-10-06 11:54:56 -0700

    Task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,13 +16,13 @@ int create_file(const char *filename, char *text_content)
 	{
 		return (-1);
 	}
+	opn = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wrt = write(opn, text_content, brd);
 	if (text_content != NULL)
 	{
 		for (brd = 0; text_content[brd];)
 			brd++;
 	}
-	opn = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wrt = write(opn, text_content, brd);
 	if (opn == -1 || wrt == -1)
 		return (-1);
 
