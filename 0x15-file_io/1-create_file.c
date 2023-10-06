ea54ad7bfd88394d3aba958e28c3a408d03cf18e
Adedoyinmaryam <maryamakindele81@gmail.com>
2023-10-06 08:48:43 -0700

    Task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,5 +1,5 @@
 #include "main.h"
-/*
+/**
  * create_file - Create and write into a file
  * and copies content into it
  * @filename: thhe filename to be created
@@ -17,7 +17,6 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	}
 	file = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-
 	if (file == -1)
 	{
 		return (-1);
