b3cf0efdd3e9ad40611f65ab0967cc1a8761467e
Adedoyinmaryam <maryamakindele81@gmail.com>
2023-10-06 11:59:46 -0700

    Task

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,21 +8,20 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int opn;
-	int wrt;
-	int brd;
+	int opn, wrt, lens = 0;
 
 	if (filename == NULL)
-	{
 		return (-1);
-	}
-	opn = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
-	wrt = write(opn, text_content, brd);
+
 	if (text_content != NULL)
 	{
-		for (brd = 0; text_content[brd];)
-			brd++;
+		for (lens = 0; text_content[lens];)
+			lens++;
 	}
+
+	opn = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	wrt = write(opn, text_content, lens);
+
 	if (opn == -1 || wrt == -1)
 		return (-1);
 
